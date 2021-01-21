82c39ed4ab0ef23e7696e5a6366ba14ae7364792
zhangbin091 <zhangbin091@126.com>
2021-01-22 00:32:47 +0800

    spring新增拦截器

---
+++ spring-di/src/main/java/com/riant/interceptor/MyInterceptor.java
@@ -0,0 +1,22 @@
+package com.riant.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Classname MyInterceptor
+ * @Description TODO
+ * @Date 2021/1/21 23:55
+ * @Created by 张斌
+ */
+public class MyInterceptor  implements HandlerInterceptor {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    }
+}
