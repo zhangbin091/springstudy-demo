63bb6cd1caa6a3ce01502db30fde0c3cdf3a4497
zhangbin091 <zhangbin091@126.com>
2021-01-24 00:25:20 +0800

    BeanNameAware等aware接口，bean的生命周期

---
+++ spring-di/src/main/java/com/riant/aware/ResourceAwareService.java
@@ -0,0 +1,49 @@
+package com.riant.aware;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Classname ResourceAwareService
+ * @Description TODO
+ * @Date 2021/1/23 23:38
+ * @Created by 张斌
+ */
+@Service
+public class ResourceAwareService implements ResourceLoaderAware, BeanNameAware, BeanFactoryAware, BeanPostProcessor, InitializingBean, DisposableBean {
+    private String beanName;
+    private ResourceLoader resourceLoader;
+    private BeanFactory beanFactory;
+    public void setBeanName(String beanName) {
+        this.beanName =beanName;
+    }
+
+
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+    this.resourceLoader = resourceLoader;
+    }
+
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+    }
+
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        return null;
+    }
+
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        return null;
+    }
+
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+    public void destroy() throws Exception {
+
+    }
+}
